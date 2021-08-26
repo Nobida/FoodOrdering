237b91a9f2bbce63ee6010d8560684b80bb9cd55
Nobida <xiyuwangkaixi@yahoo.co.jp>
2021-08-26 21:24:15 +0900

    add noLocation Alert  26/08/2021

--- FoodOrder/Model/Home.swift
+++ FoodOrder/Model/Home.swift
@@ -14,46 +14,63 @@ struct Home: View {
     
     var body: some View {
         
-        VStack(spacing: 10) {
-            HStack(spacing: 15) {
-                Button(action: {}, label: {
-                    Image(systemName: "line.horizontal.3")
-                        .font(.title)
-                        .foregroundColor(.pink)
-                })
-                
-                Text("Deliver To")
-                    .foregroundColor(.black)
-                
-                Text("Apple")
-                    .font(.caption)
-                    .fontWeight(.heavy)
-                    .foregroundColor(.pink)
-                
-                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
-            }
-            .padding([.horizontal,.top])
-            Divider()
-            HStack(spacing: 15){
-                TextField("Search", text: $HomeModel.search)
-                if HomeModel.search != "" {
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Image(systemName: "magnifyingglass")
-                            .font(.title2)
-                            .foregroundColor(.gray)
+        ZStack {
+            VStack(spacing: 10) {
+                HStack(spacing: 15) {
+                    Button(action: {}, label: {
+                        Image(systemName: "line.horizontal.3")
+                            .font(.title)
+                            .foregroundColor(.pink)
                     })
-                    .animation(.easeIn)
+                    
+                    Text(HomeModel.userLocation == nil ? "Locating..." : "Deliver To")
+                        .foregroundColor(.black)
+                    
+                    Text(HomeModel.userAddress)
+                        .font(.caption)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.pink)
+                    
+                    Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
                 }
-            }.padding(.horizontal)
-            .padding(.top,10)
+                .padding([.horizontal,.top])
+                Divider()
+                HStack(spacing: 15){
+                    TextField("Search", text: $HomeModel.search)
+                    if HomeModel.search != "" {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Image(systemName: "magnifyingglass")
+                                .font(.title2)
+                                .foregroundColor(.gray)
+                        })
+                        .animation(.easeIn)
+                    }
+                }.padding(.horizontal)
+                .padding(.top,10)
+                
+                Divider()
+                Spacer()
+            }
             
-            Divider()
-            Spacer()
+            if HomeModel.noLocation {
+                Text("Please Enable Location Access In Setting To Further Move On !!!")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width - 100, height: 120)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.black.opacity(0.3).ignoresSafeArea())
+
+            }
+            
         }
+        
+        
         .onAppear(perform: {
             // calling location delegate
-            HomeModel.locationManager.delegate = HomeModel 
-            HomeModel.locationManager.requestWhenInUseAuthorization()
+            HomeModel.locationManager.delegate = HomeModel
+            
+            
             // Modifying Info.plist
         })
     }
--- FoodOrder/ViewModel/HomeViewModel.swift
+++ FoodOrder/ViewModel/HomeViewModel.swift
@@ -13,20 +13,54 @@ class HomeViewModel: NSObject,ObservableObject, CLLocationManagerDelegate{
     @Published var search = ""
     @Published var locationManager = CLLocationManager()
     
+    // Location Details....
+    @Published var userLocation: CLLocation!
+    @Published var userAddress = ""
+    @Published var noLocation = false
+    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         //checking Location Access.....
-        print(manager.authorizationStatus)
+
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:
             print("authorized")
+            self.noLocation = false
+            manager.requestLocation()
         case .denied:
             print("denied")
+            self.noLocation = true
         default:
             print("unknown")
+            self.noLocation = false
+            //Direct Call
+            locationManager.requestWhenInUseAuthorization()
+            
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.userLocation = locations.last
+        self.extractLocation()
+        
+        //reading User Location And Extracting Details...
+    }
+    
+    func extractLocation() {
+        CLGeocoder().reverseGeocodeLocation(self.userLocation) {(res, err) in
+            guard let safeData = res else { return }
+            var address = " "
+            address += safeData.first?.name ?? ""
+            address += ", "
+            address += safeData.first?.locality ?? ""
+            self.userAddress = address
+            print(safeData)
+            
+        }
+    }
+    
+
 }
